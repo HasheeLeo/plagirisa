20379ca8b5887ddb0828d52c353f9ac9c5e7744e
Hashir Ahmad <hashirahmadleo@gmail.com>
2017-12-19 19:01:39 +0500

    Implement basic layout of controls in main window
    Base controls (like wxTextCtrl) are used to test the layout appearance.

--- Plagirisa/PFrame.cpp
+++ Plagirisa/PFrame.cpp
@@ -9,9 +9,26 @@
 #include "stdafx.hpp"
 #include "PFrame.hpp"
 
+#include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/textctrl.h>
+
 PFrame::PFrame(const wxString &title, const wxPoint &pos, const wxSize &size,
 	long style)
 	: wxFrame(nullptr, wxID_ANY, title, pos, size, style)
 {
-	// TODO: Add controls to the window here
+	wxBoxSizer *horizSizer = new wxBoxSizer(wxHORIZONTAL);
+	horizSizer->AddSpacer(15);
+	horizSizer->Add(new wxTextCtrl(this, wxID_ANY), 1, wxEXPAND);
+	horizSizer->AddSpacer(20);
+	horizSizer->Add(new wxTextCtrl(this, wxID_ANY), 1, wxEXPAND);
+	horizSizer->AddSpacer(15);
+
+	wxBoxSizer *topSizer = new wxBoxSizer(wxVERTICAL);
+	topSizer->AddSpacer(10);
+	topSizer->Add(horizSizer, 1, wxEXPAND);
+	topSizer->AddSpacer(15);
+	topSizer->Add(new wxButton(this, wxID_ANY, "Check"), 0, wxCENTER);
+	topSizer->AddSpacer(10);
+	SetSizer(topSizer);
 }

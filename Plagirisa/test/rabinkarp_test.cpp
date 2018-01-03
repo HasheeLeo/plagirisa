338f8bdda01374b8da73cb4279c59f2b923acdb6
Hashir Ahmad <hashirahmadleo@gmail.com>
2018-01-03 21:25:56 +0500

    Fix rabinkarp_test.cpp

--- Plagirisa/test/rabinkarp_test.cpp
+++ Plagirisa/test/rabinkarp_test.cpp
@@ -7,20 +7,20 @@
 *******************************************************************************/
 
 #include "rabinkarp_test.hpp"
-#include "../rabinkarp.hpp"
+#include "rabinkarp.hpp"
 
 #include <iostream>
 #include <cassert>
 #include <vector>
 
 void rabinkarp_test() {
-    const char *haystack = "The quick brown fox jumps over the lazy dog!\n";
-    std::vector<int> indices = rabinkarp(haystack, "brown");
+    const char *text1 = "The quick brown fox jumps over the lazy dog!\n";
+    std::vector<int> indices = rabinkarp(text1, "brown");
     assert(indices[0] == 10);
     assert(indices.size() == 1);
 
-	const char *haystack = "aabaacaadaabaaba";
-    indices = rabinkarp(haystack, "aaba");
+	const char *text2 = "aabaacaadaabaaba";
+    indices = rabinkarp(text2, "aaba");
     assert(indices[0] == 0);
     assert(indices[1] == 9);
 	assert(indices[2] == 12);

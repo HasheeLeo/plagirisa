5459f6078f999dc069ea84608d1759542008ad26
Hashir Ahmad <hashirahmadleo@gmail.com>
2018-01-03 21:03:34 +0500

    Update unit test for rabinkarp

--- Plagirisa/test/rabinkarp_test.cpp
+++ Plagirisa/test/rabinkarp_test.cpp
@@ -6,6 +6,8 @@
     NUST batch of 2k16, section 6C.
 *******************************************************************************/
 
+#include "../rabinkarp.hpp"
+
 #include <iostream>
 #include <cassert>
 #include <vector>
@@ -16,10 +18,12 @@ void rabinkarp_test() {
     assert(indices[0] == 10);
     assert(indices.size() == 1);
 
-    indices = rabinkarp(haystack, "tHE");
+	const char *haystack = "aabaacaadaabaaba";
+    indices = rabinkarp(haystack, "aaba");
     assert(indices[0] == 0);
-    assert(indices[1] == 31);
-    assert(indices.size() == 2);
+    assert(indices[1] == 9);
+	assert(indices[2] == 12);
+    assert(indices.size() == 3);
 
     std::cout << "rabinkarp: All tests passed.\n";
 }

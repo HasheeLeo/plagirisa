efaa5319dd5febfb38dbd6bfee98b23296b57d95
Hashir Ahmad <hashirahmadleo@gmail.com>
2018-01-06 13:52:42 +0500

    Add rabinkarp overload for single pattern search

--- Plagirisa/rabinkarp.cpp
+++ Plagirisa/rabinkarp.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <vector>
 
+// Multiple pattern overload
 std::vector<int> rabinkarp(const std::string &text,
 	const std::vector<std::string> &patterns)
 {
@@ -35,6 +36,41 @@ std::vector<int> rabinkarp(const std::string &text,
 	for (int i = 0; i < textLength - patternLength + 1; ++i) {
 		if (bloomFilter.exists(text.substr(i, patternLength)))
 			indices.push_back(i);
+	}
+	return indices;
+}
+
+// Single pattern overload
+std::vector<int> rabinkarp(const std::string &text, const std::string &pattern)
+{
+	std::vector<int> indices;
+	const int patternHash = BloomFilter::hash(pattern);
+	const int textLength = text.length();
+	const int patternLength = pattern.length();
+	int curHash = -1;
+
+	for (int i = 0; i < textLength - patternLength + 1; ++i) {
+		// If we have calculated a hash last time, slide hash
+		if (curHash != -1)
+			curHash = BloomFilter::hashroll(curHash, text[i - 1],
+				text[i + patternLength - 1], patternLength);
+		else
+			curHash = BloomFilter::hash(text.substr(i, patternLength));
+
+		// If hashes match, match the words character by character to ensure it
+		// was not a hash collision
+		if (curHash == patternHash) {
+			bool matched = true;
+			for (int j = 0; j < patternLength; ++j) {
+				if (text[i + j] != pattern[j]) {
+					matched = false;
+					break;
+				}
+			}
+
+			if (matched)
+				indices.push_back(i);
+		}
 	}
 	return indices;
 }

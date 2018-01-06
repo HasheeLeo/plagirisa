60085ab74d38c135f0d8e97de7bac0b1ffa604d7
Hashir Ahmad <hashirahmadleo@gmail.com>
2018-01-06 20:11:59 +0500

    Add another hash function in Bloom Filter
    Bloom Filter's strength lies in using multiple hash functions to reduce
    collisions. In an effort to increase accuracy, a new hash function
    "Bernstein hash" now works together with the "Rabin-Karp rolling hash" to
    insert and check entries in the Bloom Filter.
    
    With this addition, accuracy has certainly improved. To further minimze
    collisions, the size of the Bloom Filter has also been increased to a new
    prime number. The size is still under 250 bytes.

--- Plagirisa/BloomFilter.cpp
+++ Plagirisa/BloomFilter.cpp
@@ -14,21 +14,29 @@
 
 void BloomFilter::insert(const std::string &word) {
 	table[BloomFilter::hash(word) % BloomFilter::bloomSize] = 1;
+	table[BloomFilter::bhash(word) % BloomFilter::bloomSize] = 1;
 }
 
 bool BloomFilter::exists(const std::string &word)
 {
 	const size_t wordLength = word.length();
+	const char nextChar = word[wordLength - 1];
 	// If the hash has already been calculated, use hash roll
 	// This computes the new hash value from the old hash in O(1)
-	if (!lastChar)
-		curHash = BloomFilter::hash(word);
-	else
-		curHash = BloomFilter::hashroll(curHash, lastChar, word[wordLength - 1],
+	if (!lastChar) {
+		lastHash = BloomFilter::hash(word);
+		lastBhash = BloomFilter::bhash(word);
+	}
+	else {
+		lastHash = BloomFilter::hashroll(lastHash, lastChar, nextChar,
 			wordLength);
-	
+		lastBhash = BloomFilter::bhashroll(lastBhash, lastChar, nextChar,
+			wordLength);
+	}
 	lastChar = word[0];
-	return table[curHash % BloomFilter::bloomSize];
+
+	return table[lastHash % BloomFilter::bloomSize] &&
+		table[lastBhash % BloomFilter::bloomSize];
 }
 
 uint BloomFilter::hash(const std::string &word) {
@@ -36,6 +44,15 @@ uint BloomFilter::hash(const std::string &word) {
 	uint hash = 0;
 	for (int i = 0; i < wordSize; ++i)
 		hash += word[i] * pow(BloomFilter::NUM_ALPHABETS, wordSize - i - 1);
+
+	return hash;
+}
+
+// Bernstein hash
+uint BloomFilter::bhash(const std::string &word) {
+	uint hash = 0;
+	for (char c : word)
+		hash = 33 * hash + c;
 
 	return hash;
 }
--- Plagirisa/BloomFilter.hpp
+++ Plagirisa/BloomFilter.hpp
@@ -21,9 +21,11 @@ public:
 	bool exists(const std::string &word);
 
 	static uint hash(const std::string &word);
+	// Bernstein hash
+	static uint bhash(const std::string &word);
 	
-	static inline uint BloomFilter::hashroll(uint lastHash, char prevChar,
-		char nextChar, size_t patternLength)
+	static inline uint hashroll(uint lastHash, char prevChar, char nextChar,
+		size_t patternLength)
 	{
 		uint hash = lastHash - (prevChar * pow(BloomFilter::NUM_ALPHABETS,
 			patternLength - 1));
@@ -32,12 +34,19 @@ public:
 		return hash;
 	}
 
+	static inline uint bhashroll(uint lastHash, char prevChar, char nextChar,
+		size_t patternLength) {
+		return 33 * (lastHash - pow(33, patternLength - 1) * prevChar)
+			+ nextChar;
+	}
+
 	static constexpr int NUM_ALPHABETS = 127;
 private:
 	// Is a prime number
-	static constexpr int bloomSize = 997;
+	static constexpr int bloomSize = 1601;
 	std::bitset<BloomFilter::bloomSize> table = { 0 };
-	uint curHash = 0;
+	uint lastHash = 0;
+	uint lastBhash = 0;
 	char lastChar = 0;
 };
 
